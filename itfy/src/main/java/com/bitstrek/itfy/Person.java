cdf16dec47e3060952a1e9b633b302bb302c5180
Dhany <Dhany@172.20.9.187>
2015-10-03 22:30:34 +0800

    added spring boot data jpa

--- itfy/src/main/java/com/bitstrek/itfy/Person.java
+++ itfy/src/main/java/com/bitstrek/itfy/Person.java
@@ -1,17 +1,31 @@
 package com.bitstrek.itfy;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Person {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	private String name;
-	
+
 	public Person(Long id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
 	}
-	
+
 	public Person(String name) {
 		this.name = name;
+	}
+
+	public Person() {
 	}
 
 	public Long getId() {
@@ -25,7 +39,7 @@ public class Person {
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
--- itfy/src/main/java/com/bitstrek/itfy/PersonController.java
+++ itfy/src/main/java/com/bitstrek/itfy/PersonController.java
@@ -2,6 +2,7 @@ package com.bitstrek.itfy;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
+	
+	@Autowired
+	private PersonRepository personRepository;
 
 	@RequestMapping("/person")
 	public Person greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Person(counter.incrementAndGet(), String.format(template, name));
+	}
+	
+	@RequestMapping("/persons")
+	public Iterable<Person> persons() {
+		return personRepository.findAll();
 	}
 }
